1163dfa3b65381b0ccbf7286793e6775bfbef654
btate712 <btate712@gmail.com>
2020-04-01 18:47:11 -0500

    completed lab

--- View Life Cycle Lab/ViewController.swift
+++ View Life Cycle Lab/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("viewDidLoad() called")
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear(_:) called")
+    }
 }
 
